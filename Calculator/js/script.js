194bb242a920210d26d65df1d9ffacb7602f68cf
Hema Priya V <hemapriyavadivel25@gmail.com>
2022-04-13 09:23:28 +0530

    Add files via upload

---
+++ Calculator/js/script.js
@@ -0,0 +1,7 @@
+function ClearScreen(){
+    document.getElementById("solution").value="";
+}
+function input(val){
+    var x=document.getElementById("solution").value;
+    document.getElementById("solution").value=x+val;
+}
